fbeb9a3af49c261f06a3ba0b934d1a55e851be47
Sachin Gupta <sachingu1620@gmail.com>
2025-04-21 19:57:19 +0530

    sum of two num in develope branch

--- method.java
+++ method.java
@@ -2,5 +2,8 @@ public class method{
 		public static void main(String[] args){
 			System.out.println("hey Sachin how are u doing man");
 			int i=10;
+			int j=20
+			int c=i+j;
+			System.out.println(c);
 			}
 }
